9545ecfadcfd0c0dea696c62d5f510f763921f5a
TuncG <tuncgonelfb@hotmail.com>
2022-08-08 11:23:27 -0400

    struc added

--- reserveUpApp/ContentView.swift
+++ reserveUpApp/ContentView.swift
@@ -9,87 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var currentDate = Date()
-    @State private var currentTime = Date()
-    @State private var isExpanded = false
-    @State private var selectedNum = 1
     
-    @State private var isExpandedTime = false
-    @State private var selectedTime = 1
-    private var reservationTimes = ["5:00","5:30","6:00","6:30","7:00", "7:30","8:00","8:30","9:00"]
+    @State  var menuState = "people"
+    @State  var selectedNum = 1
+    @State  var currentDate = Date()
+    
+    @State  var selectedTime = "2:00"
     
     var body: some View {
         VStack(alignment: .leading, spacing: 15){
             
             Text("Reservation Page")
                 .font(.title)
+                .padding(.bottom)
             
-            Text("Select a Date").font(.title3)
-            DatePicker("", selection: $currentDate, in: Date()..., displayedComponents: .date)
+           
             
-            Text("Select a Time").font(.title3)
-            //DatePicker("", selection: $currentTime, displayedComponents: .hourAndMinute)
-            //           .labelsHidden()
+            if menuState == "people" {
+                MenuView(currentDate: currentDate, selectedNum: selectedNum, selectedTime: selectedTime)
+            }
             
-            DisclosureGroup("\(selectedTime)", isExpanded: $isExpandedTime){
-                ScrollView{
-                    VStack{
-                        ForEach(1...50,id: \.self) {num in
-                            Text("\(num)").font(.title3)
-                                .frame(maxWidth: .infinity)
-                                .padding(.all)
-                                .onTapGesture {
-                                    self.selectedTime = num
-                                    
-                                    withAnimation{
-                                        self.isExpandedTime.toggle()
-                                    }
-                                    
-                                }
-                            
-                        }
-                        
-                    }
-                    
-                }.frame(height: 150)
+          
+             Spacer()
+            Button{
                 
-            }.accentColor(.white)
-                .font(.title2)
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(8)
-            
-            
-            Text("Number of People").font(.title3)
-            DisclosureGroup("\(selectedNum)", isExpanded: $isExpanded){
-                ScrollView{
-                    VStack{
-                        ForEach(1...50,id: \.self) {num in
-                            Text("\(num)").font(.title3)
-                                .frame(maxWidth: .infinity)
-                                .padding(.all)
-                                .onTapGesture {
-                                    self.selectedNum = num
-                                    
-                                    withAnimation{
-                                        self.isExpanded.toggle()
-                                    }
-                                    
-                                }
-                            
-                        }
-                        
-                    }
-                    
-                }.frame(height: 150)
+                print("hello world2")
+                if menuState == "People" {
+                    print("hello world1")
+                    self.menuState = "Info"
+                }
                 
-            }.accentColor(.white)
-                .font(.title2)
-                .foregroundColor(.white)
-                .padding()
-                .background(Color.blue)
-                .cornerRadius(8)
+            } label: {
+                Text("Next")
+                    .font(.title2)
+                    .bold()
+            }
+            .padding()
+            .foregroundColor(.white)
+            
+            .frame(minWidth: 0 , maxWidth: .infinity)
+            .background(.blue)
+            .cornerRadius(8)
+            
             
             
             Spacer()
@@ -97,6 +58,95 @@ struct ContentView: View {
             
     }
 }
+
+//Drop down menu struct
+struct MenuView: View {
+    @State  var currentDate : Date
+    @State  var isExpanded = false
+    @State  var selectedNum : Int
+    
+    @State  var isExpandedTime = false
+    @State  var selectedTime : String
+    
+     var reservationTimes = ["5:00","5:30","6:00","6:30","7:00", "7:30","8:00","8:30","9:00"]
+    
+    var body: some View {
+        
+        Text("Select a Date").font(.title3)
+        DatePicker("", selection: $currentDate, in: Date()..., displayedComponents: .date)
+            
+            .scaleEffect(CGSize(width: 1.2, height: 1.2),anchor: .trailing)
+        
+        //People drop down below
+        Text("Number of People").font(.title3)
+        DisclosureGroup("\(selectedNum)", isExpanded: $isExpanded){
+            ScrollView{
+                VStack{
+                    ForEach(1...50,id: \.self) {num in
+                        Text("\(num)").font(.title3)
+                            .frame(maxWidth: .infinity)
+                            .padding(.all)
+                            .onTapGesture {
+                                self.selectedNum = num
+                                
+                                withAnimation{
+                                    self.isExpanded.toggle()
+                                }
+                                
+                            }
+                        
+                    }
+                    
+                }
+                
+            }.frame(height: 150)
+            
+        }.accentColor(.white)
+            .font(.title2)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(8)
+        
+        
+        // Time drop down below
+        Text("Select a Time").font(.title3)
+
+        
+        DisclosureGroup("\(selectedTime)", isExpanded: $isExpandedTime){
+            ScrollView{
+                VStack{
+                    ForEach(reservationTimes,id: \.self) {num in
+                        Text("\(num)").font(.title3)
+                            .frame(maxWidth: .infinity)
+                            .padding(.all)
+                            .onTapGesture {
+                                self.selectedTime = num
+                                
+                                withAnimation{
+                                    self.isExpandedTime.toggle()
+                                }
+                                
+                            }
+                        
+                    }
+                    
+                }
+                
+            }.frame(height: 150)
+            
+        }.accentColor(.white)
+            .font(.title2)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(8)
+    }
+    
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
